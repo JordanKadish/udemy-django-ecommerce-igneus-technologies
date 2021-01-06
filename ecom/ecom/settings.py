b97479583557135219e057d481848b78dcdeee15
Jordan Kadish <jordankadish93@gmail.com>
2021-01-06 15:22:26 +0200

    Added CORS middleware and debug allowed hosts

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+_bxds*c$ndtv-yi*_e-01k$x6o=8qo!f0kv2ce5m45%$f($k-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
